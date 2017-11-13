788e137e0a570d5e22a5799082c655dcbf7e894c
Rafal Kwiecien <Rafsters@interia.pl>
2017-11-13 01:20:07 +0100

    Diamond finished

--- src/Diamond.java
+++ src/Diamond.java
@@ -2,38 +2,38 @@ class Diamond {
     public static void main(String[] args) {
         print(23);
     }
+
     public static String print(int n) {
         StringBuilder diamond = new StringBuilder();
         int x = 1;
         int w = n;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < (w-1)/2; j++) {
-                diamond.append(" ");
-            }
-            for (int k = 0; k < n-(n-x); k++) {
-                diamond.append("*");
-            }
-            for (int j = 0; j < (w-1)/2; j++) {
-                diamond.append(" ");
-            }
-            if(i + 1 == n){
-
-            }else{
+        if (n % 2 != 0 && Integer.valueOf(n) != null) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < (w - 1) / 2; j++) {
+                    diamond.append(" ");
+                }
+                for (int k = 0; k < n - (n - x); k++) {
+                    diamond.append("*");
+                }
                 diamond.append("\n");
+                if (i >= ((n - 1) / 2)) {
+                    w += 2;
+                } else {
+                    w -= 2;
+                }
+                if (i >= ((n - 1) / 2)) {
+                    x -= 2;
+                } else {
+                    x += 2;
+                }
             }
-            if(i  >= ((n-1)/2)){
-                w += 2;
-            } else {
-                w -= 2;
-            }
-            if(i  >= ((n-1)/2)){
-                x -= 2;
-            } else{
-                x += 2;
-            }
+            System.out.println(diamond.toString());
+
+            return diamond.toString();
+
+        } else {
+            return null;
         }
-        System.out.println(diamond.toString());
-        return diamond.toString();
 
     }
 }
